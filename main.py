5f3370c9d7855cdb8384140f441ab71ecc913460
vmao <vinhxmao87@gmail.com>
2025-10-16 14:30:40 -0700

    added character count methods

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
-from stats import count_words, count_char
+from stats import count_words, get_char_count, sort_char_count, print_char_counts
 
 def get_book_text(file):
+    print("Analyzing book found at books/frankenstein.txt...")
     with open(file) as f:
         file_contents = f.read()
-        print(file_contents)
+        # print(file_contents)
+        print("----------- Word Count ----------")
         count_words(file_contents)
-        count_char(file_contents)
+        print("--------- Character Count -------")
+        char_count = get_char_count(file_contents)
+        sorted_char_count = sort_char_count(char_count)
+        print_char_counts(sorted_char_count)
+
 
 
 def main():
+    print("============ BOOKBOT ============")
     get_book_text("books/frankenstein.txt")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -2,7 +2,8 @@ def count_words(content):
     content_split = content.split()
     print(f"Found {len(content_split)} total words")
 
-def count_char(content):
+
+def get_char_count(content):
     char_count = {}
     new_content = content.lower()
     for char in new_content:
@@ -11,4 +12,27 @@ def count_char(content):
         else:
             char_count[char] = 1
 
-    print(char_count)
+    return char_count
+
+
+def sort_on(items):
+    return items["num"]
+
+
+def sort_char_count(char_dict):
+    final_dict = []
+    for char in char_dict:
+        temp_dict = {}
+        if char.isalpha():
+            temp_dict["char"] = char
+            temp_dict["num"] = char_dict[char]
+            final_dict.append(temp_dict)
+    
+    final_dict.sort(reverse=True, key=sort_on)
+    return final_dict
+
+
+def print_char_counts(dict_list):
+    for dict in dict_list:
+        print(f"{dict["char"]}: {dict["num"]}")
+
